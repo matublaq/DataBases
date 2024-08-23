9c141b239eb8a1dc986cff21e821c9aefa8f7753
Matublaq <mati.blaquier@gmail.com>
2024-08-23 20:51:35 +0200

    Stock update

--- StockMarket/SQL_functions.py
+++ StockMarket/SQL_functions.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 import sqlite3
+import shutil
 
 ###############################################################################################################
 #################################Creating StockMarket database tables##########################################
-def create_tables():
+def create_database():
     #Connect to the database
     conn = sqlite3.connect('../StockMarket.db')
     cursor = conn.cursor()
@@ -51,66 +52,75 @@ def create_tables():
     conn.close()
 
 
-###############################################################################################################
+########################################################################################################
 #############################Insert data into database##################################################
-def new_company(tickers):
-    tickers = list(tickers.upper().replace(' ', '').split(','))
-    for ticker in tickers:
-        company_id = insert_company(ticker)
-        if company_id is None:
-            pass
-        else:
-            insert_stock_quotes(company_id)
-    company_id = None
-
-
-#############################
 def insert_company(ticker):
     company_info = yf.Ticker(ticker).info 
     #Is in yfinance?
-    if not company_info or 'shortName' not in company_info: #No
-        print(f"El ticker {ticker} no existe en yfinance")
-        return None
+    if not company_info or 'shortName' not in company_info:
+        return f"El ticker {ticker} no está en yfinance"
+    
     company_name = company_info['shortName']
-
     #SQL
     with sqlite3.connect('../StockMarket.db', timeout=15) as conn: #Database connection
         cursor = conn.cursor()
         
-        cursor.execute("SELECT id FROM Companies WHERE ticker = ?", (ticker,)) #Is in the database?
+        #Is in the database?
+        cursor.execute("SELECT id FROM Companies WHERE ticker = ?", (ticker,))
         result = cursor.fetchone()
         if result: #Yes
             company_id = result[0]
-            print("Ya esta en la base de datos")
+            return f"El ticker {ticker} ya esta en la base de datos"
         else: #No
             cursor.execute("INSERT INTO Companies (name, ticker) VALUES (?, ?)", (company_name, ticker))
             company_id = cursor.lastrowid
-    return company_id
-
-#Insert data into Stock_quotes table
-def insert_stock_quotes(company_id):
-    with sqlite3.connect('../StockMarket.db', timeout=15) as conn: #Database connection 
-        cursor = conn.cursor()
         
-        cursor.execute("SELECT company_id FROM Stock_quotes WHERE company_id = ?", (company_id,)) #Is in the database?
-        result = cursor.fetchone()
-        if result: #Yes
-            print("Ya esta en la base de datos")
-            company_id = result[0]
-        else: #No
-            #Get ticker from Companies table
-            cursor.execute("SELECT ticker FROM Companies WHERE id = ?", (company_id,))
-            ticker = cursor.fetchone()[0]
-            #Download stock data
-            data = yf.download(ticker)    
+        #Get ticker from Companies table
+        cursor.execute("SELECT ticker FROM Companies WHERE id = ?", (company_id,))
+        ticker = cursor.fetchone()[0]
+        #Download stock data
+        data = yf.download(ticker)    
 
-            #Insert data into Stock_quotes table
-            for date, row in data.iterrows():
-                cursor.execute("INSERT INTO Stock_quotes  (company_id, date, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?)", 
-                                                            (company_id, date.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
+        #Insert data into Stock_quotes table
+        for date, row in data.iterrows():
+            cursor.execute("INSERT INTO Stock_quotes  (company_id, date, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                                                        (company_id, date.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
         conn.commit()
-        return company_id
+        return f"{ticker} ha sido agregado a la base de datos"
 
+###############################################################################################################
+###################################Deelete all rows from the tables############################################
+def delete_database():
+    #Connect to the database
+    conn = sqlite3.connect('../StockMarket.db')
+    cursor = conn.cursor()
+    
+    #Drop tables
+    cursor.execute("DROP TABLE IF EXISTS Companies")
+    cursor.execute("DROP TABLE IF EXISTS Stock_quotes")
+    cursor.execute("DROP TABLE IF EXISTS Idx")
+
+    #Confirm change and close connection
+    conn.commit()
+    conn.close()
+
+def delete_data():
+    #Connect to the database
+    conn = sqlite3.connect('../StockMarket.db')
+    cursor = conn.cursor()
+
+    #Delete all rows from the tables
+    cursor.execute("DELETE FROM Companies")
+    cursor.execute("DELETE FROM Stock_quotes")
+    cursor.execute("DELETE FROM Idx")
+
+##############################################################################################################
+###############################################BACKUP#########################################################
+def backup_database(file_path):
+    shutil.copyfile(file_path, file_path[3:-3] + 'backup.db')
+
+########################################################################################################
+#####################################UPDATE STOCK QUOTES################################################
 #Update stock quotes
 def update_stock_quotes():
     conn = sqlite3.connect('StockMarket_test.db')#'../StockMarket.db'
@@ -151,25 +161,5 @@ def update_stock_quotes():
         else:
             print(f"{ticker} no tiene datos")
 
-    conn.commit()
-    conn.close()
-###############################################################################################################
-###################################Deelete all rows from the tables############################################
-def delete_all():
-    #Connect to the database
-    conn = sqlite3.connect('../StockMarket.db')
-    cursor = conn.cursor()
-
-    #Delete all rows from the tables
-    #cursor.execute("DELETE FROM Companies")
-    #cursor.execute("DELETE FROM Stock_quotes")
-    #cursor.execute("DELETE FROM Idx")
-    
-    #Drop tables
-    cursor.execute("DROP TABLE IF EXISTS Companies")
-    cursor.execute("DROP TABLE IF EXISTS Stock_quotes")
-    cursor.execute("DROP TABLE IF EXISTS Idx")
-
-    #Confirm change and close connection
     conn.commit()
     conn.close()
